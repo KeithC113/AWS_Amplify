69b6cc6c98c6aa6b2b967632fe057c04c7a105d3
KeithC113 <campbell_ka@yahoo.co.uk>
2021-02-25 15:56:19 +0000

    amends to app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> Hello from V2 </h1>
+        <h1> Hello from the surface of Marssss </h1>
       </header>
     </div>
   );
